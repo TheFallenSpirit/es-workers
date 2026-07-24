eccc9e62b420832c3bf7eb1ece597c44c89ece31
Liam L <thefallenspirit@outlook.com>
2026-07-23 23:44:10 -0700

    Migrate deprecated "new" update query option

--- src/store/task.ts
+++ src/store/task.ts
@@ -4,7 +4,7 @@ import { redis, replacer } from './index.js';
 import { seconds } from 'itty-time';
 
 export async function updateTask(taskId: number, query: UpdateQuery<TaskI>): Promise<TaskI> {
-    const task = await Task.findByIdAndUpdate(taskId, query, { new: true });
+    const task = await Task.findByIdAndUpdate(taskId, query, { returnDocument: 'after' });
     if (!task) throw new Error(`Task not found -- ${taskId}`);
 
     await redis.set(`es_task:${task.user}:${task.fId}`, JSON.stringify(task, replacer), 'EX', seconds('1 week'));
