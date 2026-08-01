35000d0a1ad69be41ee2dc6a136e1e1023ea791c
Liam L <thefallenspirit@outlook.com>
2026-08-01 16:57:28 -0700

    Add uncache use rand guild commands

---
+++ src/workers/admin/commands/uncache/guild.ts
@@ -0,0 +1,34 @@
+import { CommandContext, createStringOption, Declare, Options, SubCommand } from 'seyfert';
+import { redis } from '../../../../store/index.js';
+
+const options = {
+    guild: createStringOption({
+        required: true,
+        min_length: 17,
+        max_length: 19,
+        description: ''
+    })
+};
+
+@Declare({
+    name: 'guild',
+    aliases: ['g'],
+    description: `Clear a guild's data from the cache.`
+})
+
+@Options(options)
+
+export default class extends SubCommand {
+    run = async (context: CommandContext<typeof options>) => {
+        const guildId = context.options.guild;
+        const guild = await context.client.guilds.fetch(guildId).catch(() => undefined);
+
+        if (!guild) {
+            const isCached = await redis.exists(`es_guild:${guildId}`) === 1;
+            if (!isCached) return context.editOrReply({ content: `guild isn't cached or using the bot.` });
+        };
+
+        await redis.del(`es_guild:${guildId}`);
+        await context.editOrReply({ content: `Cleared cached data from ${guild?.name ?? `\`${guildId}\``}.` });
+    };
+};
---
+++ src/workers/admin/commands/uncache/user.ts
@@ -0,0 +1,25 @@
+import { CommandContext, createUserOption, Declare, Options, SubCommand } from 'seyfert';
+import { redis } from '../../../../store/index.js';
+
+const options = {
+    user: createUserOption({
+        required: true,
+        description: ''
+    })
+};
+
+@Declare({
+    name: 'user',
+    aliases: ['u'],
+    description: `Clear a user's data from the cache.`
+})
+
+@Options(options)
+
+export default class extends SubCommand {
+    run = async (context: CommandContext<typeof options>) => {
+        const user = context.options.user;
+        await redis.del(`es_profile:${user.id}`);
+        await context.editOrReply({ content: `Cleared cache data from @${user.username}.` });
+    };
+};
